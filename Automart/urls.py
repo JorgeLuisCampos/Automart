04de476e2920057131fee8664d30ba0c020504a5
Jorge Luis Campos M <jlcampos71@gmail.com>
2019-09-21 11:25:09 -0500

    Creación de main_app y cambio en views

--- Automart/urls.py
+++ Automart/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+# from django.urls import path
+# from main_app import views # Se importa la aplicación (importa sólo la función que especificamos)
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main_app.urls')),
+    # path('index/', views.index), # se cambia por la línea de arriba
 ]
