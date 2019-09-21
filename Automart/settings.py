3a64eb2333b52bd3e54091ae53d85023d696aa8e
Jorge Luis Campos M <jlcampos71@gmail.com>
2019-09-21 11:51:30 -0500

    Agregar un template para el index

--- Automart/settings.py
+++ Automart/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app', # Esta lista tiene precedencia, por eso se pone hasta arriba
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse  # ya no se necesita, ya que se usa el template
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1> Bienvendio a Automart </h1")
+    return render(request,'index.html')
+    # return HttpResponse("<h1> Bienvendio a Automart </h1")  # ya no se necesita, ya que se usa el template
